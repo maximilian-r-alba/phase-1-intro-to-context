c6cce9f1082e54897b21612d7110f7a72a9736f5
Maximilian Alba <albam476@gmail.com>
2022-05-30 18:18:00 -0400

    commit tree f72ab5e2b1c95af08cf127b5c5ee754e743a3404

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+function createEmployeeRecord(array){
+    let punchCard = {}
+    punchCard['firstName'] = array[0]
+    punchCard['familyName'] = array[1]
+    punchCard['title'] = array[2]
+    punchCard['payPerHour'] = array [3]
+    punchCard['timeInEvents'] = []
+    punchCard['timeOutEvents'] = []
+    return punchCard
+}
+
+function createEmployeeRecords(nestedArrays){
+    return nestedArrays.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(employee, dateStamp){
+    let timeInObj = {'type': 'TimeIn', 'date': dateStamp.slice(0,-5), 'hour': parseInt(dateStamp.slice(-4))}
+    employee['timeInEvents'].push(timeInObj)
+    return employee
+    
+}
+
+function createTimeOutEvent(employee, dateStamp){
+    let timeOutObj = {'type': 'TimeOut', 'date': dateStamp.slice(0,-5), 'hour': parseInt(dateStamp.slice(-4))}
+    employee['timeOutEvents'].push(timeOutObj)
+    return employee
+    
+}
+
+function hoursWorkedOnDate (employee, date){
+    let startTime
+    let endTime
+    for (e of employee['timeInEvents']){
+        if (e['date'] === date){
+            startTime = e['hour']
+        }
+    }
+    for (e of employee['timeOutEvents']){
+        if (e['date'] === date){
+            endTime = e['hour']
+        }
+    }
+    
+    
+    return endTime - startTime
+}
